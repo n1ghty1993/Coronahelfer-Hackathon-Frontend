726baa821c28326c5aa6e65ccbcb9864c545fcaa
Pascal Schlicht <post@pascalschlicht.de>
2020-03-21 13:55:23 +0100

    Added base template, styling and font

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './style.scss';
+
+const Layout: FC = () => {
+  return (
+    <footer>
+      <span>IMPRESSUM / DATENSCHUTZ</span>
+    </footer>
+  );
+};
+
+export default Layout;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import Nav from '../Nav';
+import Footer from '../Footer';
+
+interface IProps {
+  authenticated: Boolean;
+}
+
+const Layout: FC<IProps> = ({ children, authenticated }) => {
+  return (
+    <div className="layout">
+      <Nav authenticated={authenticated} />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
