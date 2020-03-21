77a2ecb2b371d741325353d14b807e95ed64c9b8
wildsurf <wildsurf.now@googlemail.com>
2020-03-21 20:41:38 +0100

    Making a api call function

---
+++ src/api/requests.tsx
@@ -0,0 +1,26 @@
+import { IAuthContext } from '../components/App';
+
+export const callApi = async (
+  url = '',
+  auth?: IAuthContext,
+  data = {},
+  method = 'GET',
+) => {
+  if (!auth) {
+    return;
+  }
+
+  const server = process.env.REACT_APP_SERVER_URL;
+  const response = await fetch(`${server}/api/v1${url}`, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Access-Token': auth.auth.token,
+    },
+    body:
+      method === 'POST' || method === 'PUT' ? JSON.stringify(data) : undefined, // body data type must match "Content-Type" header
+  });
+  return await response.json(); // parses JSON response into native JavaScript objects
+};
--- src/pages/HelpWanted/index.tsx
+++ src/pages/HelpWanted/index.tsx
@@ -1,39 +1,33 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useContext } from 'react';
 
 import './style.scss';
 
+import { callApi } from '../../api/requests';
+
+import { Auth, IAuthContext } from '../../components/App';
 import Body from '../../components/Body';
 import Header from '../../components/Header';
 import Request from './components/Request';
 
 import { IRequest } from './components/Request/types';
 
-const callApi = async (url = '', data = {}, method = 'GET') => {
-  const response = await fetch(`http://localhost:3000/api/v1${url}`, {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Access-Token':
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTc2MTFhN2MwNmE5NjI0NjBjNWMzMWQiLCJlbWFpbCI6ImFydW5AZXhhbXBsZS5jb20iLCJpYXQiOjE1ODQ4MTAzNTksImV4cCI6MTU4NDg5Njc1OX0._LAV-6eCc0-83j_GFryk6qaCgWeCGM-7hgU5RHyyE9I',
-    },
-    body:
-      method === 'POST' || method === 'PUT' ? JSON.stringify(data) : undefined, // body data type must match "Content-Type" header
-  });
-  return await response.json(); // parses JSON response into native JavaScript objects
-};
+const HelpWanted: FC = () => {
+  const auth: IAuthContext = useContext(Auth);
 
-const Nav: FC = () => {
   const [requests, setRequests] = useState<Array<IRequest>>([]);
 
   useEffect(() => {
     const fetchRequests = async () => {
-      const result = await callApi('/request');
+      const result = await callApi('/request', auth);
       setRequests(result.result);
     };
-    fetchRequests();
-  }, []);
+
+    try {
+      fetchRequests();
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
   return (
     <div className="RequestPage">
@@ -44,9 +38,10 @@ const Nav: FC = () => {
         {requests.map(request => (
           <Request key={request.title} user={undefined} request={request} />
         ))}
+        {!requests.length && <article>Momentan gibt es keine Gesuche!</article>}
       </Body>
     </div>
   );
 };
 
-export default Nav;
+export default HelpWanted;
