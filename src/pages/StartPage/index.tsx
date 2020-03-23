2fa04585ee128389d3209b47a6998fde70820745
BennHi <57151596+BennHi@users.noreply.github.com>
2020-03-23 18:07:32 +0100

    Texte auf Startseite
    die Aufzählungszahlen bei der Leitsätzen-Liste müssten noch weg aber ich habs nicht hin bekommen xD sorry

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -22,17 +22,37 @@ function StartPage() {
       </Header>
       <Body>
         <article>
-          <h2>Was machen wir?</h2>
+          <h2>Portal für Nachbarschaftshilfe in deiner Region</h2>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            <i>“Aus der Region - für die Region”</i> <br/>
+            Ganz nach diesem Motto möchten wir Coronavirus-Nachbarschaftshilfe erleichtern und vermitteln. <br/><br/>
+            Worum geht es? <br/>Die aktuelle Situation stellt die Gesellschaft vor neue Herausforderungen. Das neuartige Coronavirus hat unser Zusammenleben in den vergangenen Wochen zunehmend eingeschränkt. Wir haben das Projekt CoronaHelfer ins Leben gerufen, um gemeinsam die Ausbreitung des Virus zu verlangsamen und einander dort zu unterstützen, wo es möglich ist.
+          </p>
+          <h2>Ziel und Vision der Initiative CoronaHelfer</h2>
+          <p>
+            <ul>
+              <li>CoronaHelfer schützt gefährdete Personen</li>
+              <li>CoronaHelfer entlastet Menschen in systemrelevanten Berufen (Gesundheits-, Dienstleistungssystem etc.)</li>
+              <li>CoronaHelfer unterstütz generell Hilfesuchende</li>
+              <li>Coronaviurs-Nachbarschaftshilfe für alle!</li>
+            </ul>
+          </p>
+          <h2>Leitsätze für Coronavirus-Nachbarschaftshilfe</h2>
+          <p>
+            <ul>
+              <li><b>C</b>ool bleiben</li>
+              <li><b>O</b>rdentlich Hände waschen</li>
+              <li><b>R</b>ücksichtsvoll bleiben</li>
+              <li><b>O</b>hne Hamsterkäufe</li>
+              <li><b>N</b>achbarschaftshilfe</li>
+              <li><b>A</b>bstand halten</li>
+            </ul>
+          </p>
+          <h2>Unser Hintergrund</h2>
+          <p>
+            Wie ist unsere Idee entstanden? Über soziale Netzwerke sind wir darauf aufmerksam geworden, wie unfassbar viele von euch in ihrer Umgebung Hilfe angesichts der aktuellen Situation anbieten möchten. Bei anderen Vermittlungsportalen fiel uns die große Anzahl an hilfsbereiten Menschen im Gegensatz zu nur wenigen Anfragen auf.<br/><br/>
+            Wir haben erkannt, dass die Vermittlung von Fremden an Fremde eine große Herausforderung darstellt und machten uns die Sicherheit unserer Nutzer zur Aufgabe. Auch eine verlässliche und schnelle Vermittlung sowie die Einbindung bereits bestehender Offline-Arbeit vor Ort sind uns bei der Coronavirus-Nachbarschaftshilfe wichtig.<br/><br/>
+            Dank des Hackathons #WirVsVirus der Bundesregierung haben wir ein tolles Team für unsere Idee gewinnen können. Seitdem arbeiten wir gemeinsam auf Hochtouren an der Umsetzung und dem Ausbau unserer Vermittlungsplattform
           </p>
         </article>
       </Body>
